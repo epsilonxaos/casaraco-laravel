44074d15bb46874ae9bea322517cb1a362f5d187
Jesus Glez <jesusgleztr94@gmail.com>
2024-06-29 12:20:54 -0600

    refactor: :recycle: Rediseño de Sandra

--- resources/js/react/pages/Home.jsx
+++ resources/js/react/pages/Home.jsx
@@ -1,6 +1,3 @@
-import { useTranslation } from 'react-i18next'
-import { Banner } from '../components/Banner'
-import Text from '../components/Text'
 import { Bienvenida } from './home/Bienvenida'
 import { Boardroom } from './home/Boardroom'
 import { Coahuila } from './home/Coahuila'
@@ -10,10 +7,9 @@ import { Gastronomy } from './home/Gastronomy'
 import { Habitaciones } from './home/Habitaciones'
 import { Offers } from './home/Offers'
 import { Video } from './home/Video'
+import { Sandra } from './home/Sandra'
 
 export default function Home() {
-	const { t } = useTranslation()
-
 	return (
 		<>
 			<Video />
@@ -22,22 +18,7 @@ export default function Home() {
 			<Offers />
 			<Habitaciones />
 
-			<section className='relative md:pb-20 md:pt-10'>
-				<img
-					className='mx-auto hidden h-[450px] w-[90%] max-w-[1300px] object-cover md:block'
-					src='/img/home/sandra.jpg'
-					alt=''
-				/>
-				<img
-					className='max-h-[500px] w-full object-cover md:hidden'
-					src='/img/home/sandram.jpg'
-					alt=''
-				/>
-
-				<div className='absolute left-1/2 top-1/2 w-[90%] max-w-[500px] -translate-x-1/2 -translate-y-1/2 bg-cafe bg-opacity-80 py-3 text-center text-white'>
-					<Text className='mx-auto w-4/5 max-w-[400px]'>{t('text.add1')}</Text>
-				</div>
-			</section>
+			<Sandra />
 
 			<Experiences />
 			<Gastronomy />
---
+++ resources/js/react/pages/home/Sandra.jsx
@@ -0,0 +1,38 @@
+import { useTranslation } from 'react-i18next'
+import Text from '../../components/Text'
+
+export const Sandra = () => {
+	const { t } = useTranslation()
+
+	return (
+		<section className='relative py-10 md:pb-20'>
+			<div className='mx-auto grid w-[90%] grid-cols-2 border border-cafe p-4 md:grid-cols-3'>
+				<div className='order-1 col-span-1 md:order-1'>
+					<img
+						src='/img/home/sandra.jpg'
+						className='h-[450px] object-cover'
+						alt=''
+					/>
+				</div>
+				<div className='relative order-3 col-span-2 flex items-center justify-center py-12 text-center md:order-2 md:col-span-1'>
+					<div>
+						<Text className='mx-auto mb-4 w-4/5 max-w-[400px]'>{t('text.add1')}</Text>
+						<Text className='mx-auto w-4/5 max-w-[400px]'>{t('text.add1.2')}</Text>
+					</div>
+					<img
+						src='/img/logo-menu.png'
+						alt='icono'
+						className='absolute bottom-[-58px] left-1/2 mx-auto mb-4 w-[50px] -translate-x-1/2 bg-crema md:bottom-[-76px] md:w-[85px]'
+					/>
+				</div>
+				<div className='order-2 col-span-1 md:order-3'>
+					<img
+						src='/img/home/sandra.jpg'
+						className='h-[450px] object-cover'
+						alt=''
+					/>
+				</div>
+			</div>
+		</section>
+	)
+}
